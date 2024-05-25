7e9364eb63ef822e1c195cf81b43d4b70ab33be0
blueskye16 <fabianuskevin147@gmail.com>
2024-05-25 21:53:40 +0700

    [view data] nambahin data di blog

--- resources/views/blog.blade.php
+++ resources/views/blog.blade.php
@@ -1,4 +1,26 @@
 <x-layout>
     <x-slot:title>{{ $title }}</x-slot:title>
-    <h3 class="text-xl">ma Blog Page</h3>
+
+    <article class="py-8 max-w-screen-md border-b border-gray-300">
+        <h2 class="mb-1 text-3xl tracking-tight font-bold text-gray-900">Judul Artikel 1</h2>
+        <div class="text-base text-gray-500">
+            <a href="#">Fabianus Kevin</a> | 1 May 2024
+        </div>
+        <p class="my-4 font-light">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Error corrupti, nisi
+            corporis eos aspernatur ex
+            laboriosam numquam delectus quisquam repellendus odit incidunt provident ut, accusantium consequatur quo,
+            debitis sapiente officia.</p>
+        <a href="#" class="text-medium text-blue-500 hover:underline">Read More &raquo;</a>
+    </article>
+
+    <article class="py-8 max-w-screen-md border-b border-gray-300">
+        <h2 class="mb-1 text-3xl tracking-tight font-bold text-gray-900">Judul Artikel 2</h2>
+        <div class="text-base text-gray-500">
+            <a href="#">Fabianus Kevin</a> | 2 February 2024
+        </div>
+        <p class="my-4 font-light">Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas debitis quaerat
+            eius. Molestiae necessitatibus odio ullam vero pariatur velit officiis in! Adipisci veniam perferendis et
+            alias minus error hic? Tempore.</p>
+        <a href="#" class="text-medium text-blue-500 hover:underline">Read More &raquo;</a>
+    </article>
 </x-layout>
