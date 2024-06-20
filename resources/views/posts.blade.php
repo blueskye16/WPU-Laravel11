719fd7b5640c389a74fbed52323080575c3c522f
blueskye16 <fabianuskevin147@gmail.com>
2024-06-20 19:32:06 +0700

    [Post Category]
    intro benerin route buat nyari berdasarkan author
    postingan yang dibikin oleh
    from id to username

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -2,15 +2,21 @@
     <x-slot:title>{{ $title }}</x-slot:title>
 
     @foreach ($posts as $post)
-    <article class="py-8 max-w-screen-md border-b border-gray-300">
-        <a href="/posts/{{ $post['slug'] }}">
-            <h2 class="mb-1 text-3xl tracking-tight font-bold text-gray-900 hover:underline">{{ $post['title'] }}</h2>
-        </a>
-        <div class="text-base text-gray-500">
-            <a href="/authors/{{ $post->author->id }}" class="hover:underline">{{ $post->author->name }}</a> | {{ $post->created_at->diffForHumans() }}
-        </div>
-        <p class="my-4 font-light">{{ Str::limit($post['body'], 150) }}</p>
-        <a href="/posts/{{ $post['slug'] }}" class="text-medium text-blue-500 hover:underline">Read More &raquo;</a>
-    </article>
+        <article class="py-8 max-w-screen-md border-b border-gray-300">
+            <a href="/posts/{{ $post['slug'] }}">
+                <h2 class="mb-1 text-3xl tracking-tight font-bold text-gray-900 hover:underline">{{ $post['title'] }}
+                </h2>
+            </a>
+            <div>
+                By
+                <a href="/authors/{{ $post->author->username }}"
+                    class="text-base text-gray-500 hover:underline">{{ $post->author->name }}</a>
+                in
+                <a href="#" class="text-base text-gray-500 hover:underline">Post Category</a> |
+                {{ $post->created_at->diffForHumans() }}
+            </div>
+            <p class="my-4 font-light">{{ Str::limit($post['body'], 150) }}</p>
+            <a href="/posts/{{ $post['slug'] }}" class="text-medium text-blue-500 hover:underline">Read More &raquo;</a>
+        </article>
     @endforeach
 </x-layout>
