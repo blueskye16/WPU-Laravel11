40e02307127fd2afc25ded53da1e54d5d8013ff1
blueskye16 <fabianuskevin147@gmail.com>
2024-06-21 17:59:11 +0700

    [Database Seeder]
    using seeder in migration
    jadi gausa buka php artisan tinker

---
+++ database/seeders/CategorySeeder.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Category;
+use Illuminate\Database\Seeder;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+class CategorySeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        // Category::factory(3)->create();
+        Category::create([
+            'name' => 'Front-End Website',
+            'slug' => 'front-end-Website'
+        ]);
+
+        Category::create([
+            'name' => 'Design Website',
+            'slug' => 'design-website'
+        ]);
+
+        Category::create([
+            'name' => 'Data Analyst',
+            'slug' => 'data-analyst'
+        ]);
+    }
+}
